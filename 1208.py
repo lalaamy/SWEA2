d10a78e7de985aca49ecfd02057d46e01046f7e2
lalaamy <lalaamy@naver.com>
2025-07-26 13:56:06 +0900

    1208_4

--- 1208.py
+++ 1208.py
@@ -1,13 +1,12 @@
 
-D = int(input())
-
-
-T = list(map(int, input().split()))
-Max_num = max(T)
-Min_num = min(T)
-
-
 for i in range(1, 11) :
+
+    D = int(input())
+
+    T = list(map(int, input().split()))
+    Max_num = max(T)
+    Min_num = min(T)
+
     def dump_num (num, Max_num, Min_num) :
         if Max_num-Min_num <= 1 or num == 0 :
             return Max_num - Min_num
@@ -17,6 +16,6 @@ for i in range(1, 11) :
             min_after = Min_num +1
             return dump_num(num-1, max_after, min_after)
         
-print(f'#{i+1}', dump_num(D, Max_num, Min_num))
+    print(f'#{i}', dump_num(D, Max_num, Min_num))
 
 
