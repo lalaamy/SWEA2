d80fb97a3eccb510ca6305bb417ee41939f9937e
lalaamy <lalaamy@naver.com>
2025-08-14 07:41:39 +0900

    2

--- IM 대비/20230풍선팡.py
+++ IM 대비/20230풍선팡.py
@@ -1,23 +1,23 @@
 T = int(input())
- 
+
 for t in range(1, T+1) :
     N = int(input())
     arr = [(list(map(int, input().split()))) for _ in range (N)]
- 
- 
+
+
     max_sum = 0
     for r in range (N) :
         for c in range (N) :
             total_sum = -(arr[r][c])
- 
+
             for i in range(N) :
                 total_sum += arr[r][i]
-             
+            
             for j in range(N) :
                 total_sum += arr[j][c]
- 
+
             if max_sum < total_sum :
                 max_sum = total_sum
- 
-     
+
+    
     print (f'#{t} {max_sum}')
